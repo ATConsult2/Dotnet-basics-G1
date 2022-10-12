22503653ee862066a73d8624dd195028becf8ba4
Andres <a0z9@rambler.ru>
2022-10-12 13:19:31 +0300

    day2 if test

--- TestApp2/TestFlow/Program.cs
+++ TestApp2/TestFlow/Program.cs
@@ -7,17 +7,41 @@ using static System.Console;
 
 namespace andestech.learning2022.krasn
 {
+    enum FileFlags { 
+    Read, Write, Append, Create, ReadWrite
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            WriteLine(byte.MaxValue);
-            WriteLine(short.MaxValue);
-            WriteLine(int.MaxValue);
-            WriteLine(long.MaxValue);
-            WriteLine(ulong.MaxValue);
 
-            for (;;)
+        static void accessFile(FileFlags ff, string fileName = "file1")
+        {
+            switch (ff)
+            {
+                case FileFlags.Create:
+                    WriteLine($"Create file {fileName}. ");
+                    //...
+                    break;
+                case FileFlags.Write:
+                    WriteLine($"Write to file {fileName}. ");
+                    //...
+                    break;
+                case FileFlags.Read:
+                    WriteLine($"Read from file {fileName}. ");
+                    //...
+                    break;
+                default:
+                    WriteLine("some ops...");
+                    break;
+            }
+        
+        
+        }
+
+        static void checkNumbers() {
+            WriteLine("Simple calc, press \"x\" to exit");
+
+            for (; ; )
             {
                 byte b1;
                 Write("Enter number: ");
@@ -30,10 +54,58 @@ namespace andestech.learning2022.krasn
                     b2 = 0;
                     WriteLine("Wrong value. Try again..");
                 }
-                b1 = (byte)(100 + b2);
+                //b1 = checked((byte)(100 + b2));
+                checked
+                {
+                    b1 = (byte)(100 + b2);
+                }
                 WriteLine($"result: {b1}");
                 //if (b1 == 100) break;
             }
+
+        }
+
+        static void getMaxNumbers() {
+            WriteLine(byte.MaxValue);
+            WriteLine(short.MaxValue);
+            WriteLine(int.MaxValue);
+            WriteLine(long.MaxValue);
+            WriteLine(ulong.MaxValue);
+        }
+
+        static void testFileOps() {
+            accessFile(FileFlags.Create, "file1.txt");
+            accessFile(FileFlags.Write);
+            accessFile(FileFlags.ReadWrite, "file1.txt");
+
+        }
+
+        static void testIf(int a) {
+            if (a > 10)
+            {
+                WriteLine("a>10");
+            }
+            else if (a > 2) { WriteLine("10>=a>2"); }
+            else if (a > -3) { WriteLine("2>=a>-3"); }
+            else { WriteLine("-3>=a"); }
+
+
+            WriteLine(
+                $"Program has {a} bug{((a==1)?"":"s")}!");
+         
+        }
+        static void Main(string[] args)
+        {
+            //getMaxNumbers();    
+            //checkNumbers();
+            testFileOps();
+            testIf(1);
+
+
+
+
+
+
 
 
         }
