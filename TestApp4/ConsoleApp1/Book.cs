6fd9d3219987a710efa9c14a23b17d13737c1fba
Andres <a0z9@rambler.ru>
2022-10-17 12:16:49 +0300

    inheritance test

--- TestApp4/ConsoleApp1/Book.cs
+++ TestApp4/ConsoleApp1/Book.cs
@@ -7,7 +7,49 @@ using static System.Console;
 
 namespace andestech.learning2022.krasn.library
 {
-    internal class Book
+    internal class Book : Object
     {
+        public string Author { get; private set; }
+        public string Title { get; private set; }
+        private static Random random = new Random();
+
+        private int _internal_id = random.Next(1_000_000);
+
+        public Book(string author, string title)
+        {
+            Author = author;
+            Title = title;
+        }
+
+        public void Loan() {
+            WriteLine("Loan Book.");
+        }
+        public override string ToString()
+        {
+            return $"Book: \"{Title}\" by {Author}, internalId: {_internal_id}.";
+        }
+
     }
+
+    internal class RareBook : Book{
+        public string SpecialStoreId { get; private set; }
+
+        public RareBook(string author, string title, string specialStoreId):
+            base(author, title)
+        {
+            SpecialStoreId = specialStoreId;
+        }
+
+        public new void Loan()
+        {
+            WriteLine("Loan RareBook imposible. Meet library superviser..");
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\b, sId: {SpecialStoreId}";
+        }
+
+    }
+
 }
--- TestApp4/ConsoleApp1/Program.cs
+++ TestApp4/ConsoleApp1/Program.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
+using andestech.learning2022.krasn.library;
 
 namespace andestech.learning2022.krasn
 {
     internal class Singleton { 
-      private static Singleton _instance;
+     // private static Singleton[] _instance = new Singleton[3];
+     // private int counter;
+        private static Singleton _instance;
+              
       public static Singleton Instance {
             get
             {
@@ -20,9 +24,13 @@ namespace andestech.learning2022.krasn
         private Singleton()
         {
             WriteLine("+++ Singleton: " + GetHashCode());
+         
         }
     
     }
+
+   
+
 
     internal class Program
     {
@@ -30,6 +38,16 @@ namespace andestech.learning2022.krasn
         {
             Singleton s1 = Singleton.Instance;
             Singleton s2 = Singleton.Instance;
+
+            Book book1 = new Book(author: "Jack London", title: "White silence");
+            WriteLine(book1);
+            book1.Loan();
+
+            RareBook book2 = new RareBook(author: "Leonaro da Vinci", title: "Opus 1112", "AA-dd-44-2022");
+            WriteLine(book2);
+            book2.Loan();
+
+            //new Singleton(); private constructor!!!
 
 
         }
