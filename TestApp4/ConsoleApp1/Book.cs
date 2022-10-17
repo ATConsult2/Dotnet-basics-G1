8ef68ac98468fe7767c4d2c2047a9576a79bcd2f
Andres <a0z9@rambler.ru>
2022-10-17 11:17:09 +0300

    singleton test

---
+++ TestApp4/ConsoleApp1/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.learning2022.krasn.library
+{
+    internal class Book
+    {
+    }
+}
---
+++ TestApp4/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace andestech.learning2022.krasn
+{
+    internal class Singleton { 
+      private static Singleton _instance;
+      public static Singleton Instance {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+                        }
+
+        private Singleton()
+        {
+            WriteLine("+++ Singleton: " + GetHashCode());
+        }
+    
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+
+        }
+    }
+}
