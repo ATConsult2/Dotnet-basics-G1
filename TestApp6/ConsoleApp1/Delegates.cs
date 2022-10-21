b5bd5acb46bd8ef2d7c510b6293316d84f020960
Andres <a0z9@rambler.ru>
2022-10-21 11:09:16 +0300

    Lambdas Dict

--- TestApp6/ConsoleApp1/Delegates.cs
+++ TestApp6/ConsoleApp1/Delegates.cs
@@ -26,12 +26,47 @@ namespace andestech.learning2022.krasn
             del(i);
         }
 
+        static void TestDelegate2(Action<int> del, int i)
+        {
+            //.......
+            del(i);
+        }
+
         static void Main(string[] args)
         {
             TestDelegate1(F1, 10);
             TestDelegate1(F2, 20);
+            TestDelegate2(F1, 10);
+            TestDelegate2(F2, 20);
 
+            WriteLine("------------------------------");
+            Action<int> d1 = F1;
+            d1 += F2;
+            TestDelegate2(d1, 20);
+            d1 -= F1;
+            WriteLine("------------------------------");
+            TestDelegate2(d1, 20);
+            TestDelegate2(i =>
+            {
+                WriteLine($"Lambda test 1 --> {i}");
+            },11);
 
+            Action<int> L2 = 
+                i => WriteLine($"Lambda test 2 --> {i}");
+            d1 += L2;
+            TestDelegate2(d1, -12);
+
+            WriteLine("---------- Dictionary lambda test ------------");
+            Dictionary<string, Func<double, double, double>> calculus =
+                new Dictionary<string, Func<double, double, double>>()
+                {
+                    {"+", (x,y) => x+y },
+                    {"-", (x,y) => x-y },
+                    {"*", (x,y) => x*y },
+                    {"/", (x,y) => x/y }
+                };
+
+            WriteLine(calculus["/"](1,5));
         }
     }
 }
